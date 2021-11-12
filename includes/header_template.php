ef2ac5ec5ac11112f7f676d3106f019e70d7f868
Christopher Tyrrell <43977100+cdtyrrell@users.noreply.github.com>
2021-11-12 07:19:50 -0600

    initial monkeying
    some css works on index

--- includes/header_template.php
+++ includes/header_template.php
@@ -1,22 +1,35 @@
 <script>
 	//if(top.frames.length!=0) top.location=self.document.location;
 </script>
-<table id="maintable" cellspacing="0">
-	<tr>
-		<td id="header" colspan="3">
-			<div style="clear:both; width:100%; height:170px; border-bottom:1px solid #000000;">
-				<div style="float:left">
-					<img src="<?php echo $CLIENT_ROOT; ?>/images/layout/left_logo.jpg" style="margin:0px 30px;width:130px" />
-				</div>
-				<div style="margin-left: 40px; color: #fff; font-family: 'Mate', serif; letter-spacing: 1px; text-shadow: 0 0 7px rgba(0,0,0,0.5);">
-					<div style="margin-top:30px; font-size:60px; line-height:48px;">
-						First Level Title
-					</div>
-					<div style="margin-top:20px; font-size:35px; font-style: italic">
-						Second Level Title
-					</div>
-				</div>
-			</div>
+<!-- Navbar -->
+<div class="w3-top">
+ <div class="w3-bar w3-theme-d2 w3-left-align w3-large">
+  <a class="w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right w3-padding-large w3-hover-white w3-large w3-theme-d2" href="javascript:void(0);" onclick="openNav()"><i class="fa fa-bars"></i></a>
+  <!-- Home -->
+  <a href="<?php echo $clientRoot; ?>/index.php" class="w3-bar-item w3-button w3-padding-large w3-theme-d4" title="Home">Home</a>
+  <!-- icon: <i class="fa fa-home w3-margin-right"></i> -->
+  <!-- Species -->
+  <div class="w3-dropdown-hover w3-hide-small">
+    <button class="w3-button w3-padding-large" title="Search">Species</button>
+    <div class="w3-dropdown-content w3-card-4 w3-bar-block" style="width:150px">
+      <a href="<?php echo $clientRoot; ?>/checklists/checklist.php?cl=1" class="w3-bar-item w3-button">Browse</a>
+      <!-- <a href="<?php echo $clientRoot; ?>/checklists/dynamicmap.php" class="w3-bar-item w3-button">Map Search</a> -->
+      <!-- <a href="<?php echo $clientRoot; ?>/imagelib/search.php" class="w3-bar-item w3-button">Images</a> -->
+    </div>
+  </div>
+  <!-- Specimens -->
+  <div class="w3-dropdown-hover w3-hide-small">
+    <button class="w3-button w3-padding-large" title="Search">Specimens</button>
+    <div class="w3-dropdown-content w3-card-4 w3-bar-block" style="width:150px">
+      <a href="<?php echo $clientRoot; ?>/collections/index.php" class="w3-bar-item w3-button">Search</a>
+      <!-- <a href="<?php echo $clientRoot; ?>/collections/map/mapinterface.php" class="w3-bar-item w3-button">Map Search</a> -->
+      <!-- <a href="<?php echo $clientRoot; ?>/imagelib/search.php" class="w3-bar-item w3-button">Images</a> -->
+    </div>
+  </div>
+  <!-- Keys -->
+  <!-- <a href="<?php echo $clientRoot; ?>/ident/index.php" class="w3-bar-item w3-button w3-hide-small w3-padding-large w3-hover-white" title="Keys">Keys</a> -->
+
+
 			<div id="top_navbar">
 				<div id="right_navbarlinks">
 					<?php
@@ -25,88 +38,59 @@
 						<span style="">
 							Welcome <?php echo $USER_DISPLAY_NAME; ?>!
 						</span>
-						<span style="margin-left:5px;">
-							<a href="<?php echo $CLIENT_ROOT; ?>/profile/viewprofile.php">My Profile</a>
-						</span>
-						<span style="margin-left:5px;">
-							<a href="<?php echo $CLIENT_ROOT; ?>/profile/index.php?submit=logout">Logout</a>
-						</span>
-						<?php
+						<div class="w3-dropdown-hover w3-hide-small w3-right">
+					    <button class="w3-button w3-padding-large" title="My Account">My Account</button>
+					    <div class="w3-dropdown-content w3-card-4 w3-bar-block" style="width:100px">
+					      <a href="<?php echo $clientRoot; ?>/profile/viewprofile.php" class="w3-bar-item w3-button">Profile</a>
+					      <a href="<?php echo $clientRoot; ?>/profile/index.php?submit=logout" class="w3-bar-item w3-button">Logout</a>
+					      <a href='<?php echo $clientRoot; ?>/sitemap.php' class="w3-bar-item w3-button">Site Menu</a>
+					    </div>
+					  </div>
+<?php
 					}
 					else{
 						?>
-						<span style="">
-							<a href="<?php echo $CLIENT_ROOT.'/profile/index.php?refurl='.$_SERVER['SCRIPT_NAME'].'?'.htmlspecialchars($_SERVER['QUERY_STRING'], ENT_QUOTES); ?>">
-								Log In
-							</a>
-						</span>
-						<span style="margin-left:5px;">
-							<a href="<?php echo $CLIENT_ROOT; ?>/profile/newprofile.php">
-								New Account
-							</a>
-						</span>
+						<div class="w3-dropdown-hover w3-hide-small w3-right">
+					    <button class="w3-button w3-padding-large" title="Account">Accounts</button>
+					    <div class="w3-dropdown-content w3-card-4 w3-bar-block" style="width:100px">
+
+					      <a href="<?php echo $clientRoot."/profile/index.php?refurl=".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']; ?>" class="w3-bar-item w3-button" title="Log In">Log In</a>
+					      <a href="<?php echo $clientRoot; ?>/profile/newprofile.php" class="w3-bar-item w3-button">Sign Up</a>
+					    </div>
+					  </div>
+
 						<?php
 					}
-					?>
-					<span style="margin-left:5px;margin-right:5px;">
-						<a href='<?php echo $CLIENT_ROOT; ?>/sitemap.php'>Sitemap</a>
-					</span>
+						?>
+					</div>
+				 </div>
 
-				</div>
-				<ul id="hor_dropdown">
-					<li>
-						<a href="<?php echo $CLIENT_ROOT; ?>/index.php" >Home</a>
-					</li>
-					<li>
-						<a href="#" >Search</a>
-						<ul>
-							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/collections/index.php" >Search Collections</a>
-							</li>
-							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/collections/map/index.php" target="_blank">Map Search</a>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<a href="#" >Images</a>
-						<ul>
-							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/imagelib/index.php" >Image Browser</a>
-							</li>
-							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/imagelib/search.php" >Search Images</a>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php" >Inventories</a>
-						<ul>
-							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=1" >Project 1</a>
-							</li>
-							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=2" >Project 2</a>
-							</li>
-							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/projects/index.php?pid=3" >Project 3</a>
-							</li>
-						</ul>
-					</li>
-					<li>
-						<a href="#" >Interactive Tools</a>
-						<ul>
-							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/checklists/dynamicmap.php?interface=checklist" >Dynamic Checklist</a>
-							</li>
-							<li>
-								<a href="<?php echo $CLIENT_ROOT; ?>/checklists/dynamicmap.php?interface=key" >Dynamic Key</a>
-							</li>
-						</ul>
-					</li>
-				</ul>
-			</div>
-		</td>
-	</tr>
-	<tr>
-		<td id='middlecenter'  colspan="3">
+         <!-- Navbar on small screens -->
+         <div id="navDemo" class="w3-bar-block w3-theme-d2 w3-hide w3-hide-large w3-hide-medium w3-large">
+           <a href="<?php echo $clientRoot; ?>/index.php" class="w3-bar-item w3-button w3-padding-large">Home</a>
+           <a href="#" class="w3-bar-item w3-button w3-padding-large">Browse Species</a>
+           <!-- <a href="<?php echo $clientRoot; ?>/checklists/dynamicmap.php?interface=key" class="w3-bar-item w3-button w3-padding-large">Species Map Search</a> -->
+           <a href="<?php echo $clientRoot; ?>/collections/index.php" class="w3-bar-item w3-button w3-padding-large">Search Specimens</a>
+           <!-- <a href="<?php echo $clientRoot; ?>/collections/map/mapinterface.php" class="w3-bar-item w3-button w3-padding-large">Specimen Map Search</a>
+           <a href="#" class="w3-bar-item w3-button w3-padding-large">Keys</a>
+           <a href="<?php echo $clientRoot; ?>/imagelib/search.php" class="w3-bar-item w3-button w3-padding-large">Image Search</a> -->
+
+           <?php
+           if($userDisplayName){
+           ?>
+             <a href="<?php echo $clientRoot; ?>/sitemap.php" class="w3-bar-item w3-button w3-padding-large">Site Menu</a>
+             <a href="<?php echo $clientRoot; ?>/profile/viewprofile.php" class="w3-bar-item w3-button w3-padding-large">Profile</a>
+             <a href="<?php echo $clientRoot; ?>/profile/index.php?submit=logout" class="w3-bar-item w3-button w3-padding-large">Logout</a>
+           <?php
+           }
+           else{
+           ?>
+           <a href="<?php echo $clientRoot."/profile/index.php?refurl=".$_SERVER['PHP_SELF']."?".$_SERVER['QUERY_STRING']; ?>" class="w3-bar-item w3-button w3-padding-large">Log In</a>
+           <a href="<?php echo $clientRoot; ?>/profile/newprofile.php" class="w3-bar-item w3-button w3-padding-large">Sign Up</a>
+           <?php
+           }
+           ?>
+
+         </div>
+
+	<div class="w3-container w3-content" style="max-width:1400px;margin-top:80px">
--- index_template.php
+++ index_template.php
@@ -3,6 +3,7 @@ include_once('config/symbini.php');
 include_once('content/lang/index.'.$LANG_TAG.'.php');
 header("Content-Type: text/html; charset=".$CHARSET);
 ?>
+
 <html>
 <head>
 	<title><?php echo $DEFAULT_TITLE; ?> Home</title>
@@ -25,27 +26,164 @@ header("Content-Type: text/html; charset=".$CHARSET);
 		}
 	</style>
 </head>
-<body>
+<body class="w3-theme-l5">
 	<?php
 	include($SERVER_ROOT.'/includes/header.php');
 	?>
 	<!-- This is inner text! -->
-	<div id="innertext">
-		<h1></h1>
-		<div id="quicksearchdiv">
-			<!-- -------------------------QUICK SEARCH SETTINGS--------------------------------------- -->
-			<form name="quicksearch" id="quicksearch" action="<?php echo $CLIENT_ROOT; ?>/taxa/index.php" method="get" onsubmit="return verifyQuickSearch(this);">
-				<div id="quicksearchtext" ><?php echo (isset($LANG['QSEARCH_SEARCH'])?$LANG['QSEARCH_SEARCH']:'Taxon Search'); ?></div>
-				<input id="taxa" type="text" name="taxon" />
-				<button name="formsubmit"  id="quicksearchbutton" type="submit" value="Search Terms"><?php echo (isset($LANG['QSEARCH_SEARCH_BUTTON'])?$LANG['QSEARCH_SEARCH_BUTTON']:'Search'); ?></button>
-			</form>
-		</div>
-		<div style="padding: 0px 10px;">
-			Description and introduction of project
-		</div>
-	</div>
+	  <!-- The Grid -->
+	  <div class="w3-row">
+	    <!-- Left Column -->
+	    <div class="w3-col m3">
+	      <!-- Profile -->
+	      <div class="w3-card w3-round w3-white">
+	        <div class="w3-container">
+
+						<h4 class="w3-center">American Bamboos</h4>
+	          <p class="w3-center"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/97/Bamboo_DSCN2465.jpg/450px-Bamboo_DSCN2465.jpg" class="w3-circle" style="height:106px;width:106px" alt="A bamboo"></p>
+	          <p>image credit: Michele~commonswiki<p>
+	          <hr>
+	          <p>Dedicated to documenting and presenting information on the diversity and distribution of bamboo species native to the western hemisphere.</p>
+	         </div>
+	       </div>
+	       <br>
+
+	       <!-- Accordion -->
+	       <div class="w3-card w3-round">
+	         <div class="w3-white">
+	           <button onclick="myFunction('Demo1')" class="w3-button w3-block w3-theme-l1 w3-left-align">Systematics</button>
+	           <div id="Demo1" class="w3-hide w3-container">
+	             <p>Some text..</p>
+	           </div>
+	           <button onclick="myFunction('Demo2')" class="w3-button w3-block w3-theme-l1 w3-left-align">Characteristics</button>
+	           <div id="Demo2" class="w3-hide w3-container">
+	             <p>Some other text..</p>
+	           </div>
+	           <button onclick="myFunction('Demo3')" class="w3-button w3-block w3-theme-l1 w3-left-align">Publications</button>
+	           <div id="Demo3" class="w3-hide w3-container">
+	          <div class="w3-row-padding">
+	          <br>
+	            <div class="w3-half">
+	              <img src="/w3images/lights.jpg" style="width:100%" class="w3-margin-bottom">
+	            </div>
+	            <div class="w3-half">
+	              <img src="/w3images/nature.jpg" style="width:100%" class="w3-margin-bottom">
+	            </div>
+	            <div class="w3-half">
+	              <img src="/w3images/mountains.jpg" style="width:100%" class="w3-margin-bottom">
+	            </div>
+	            <div class="w3-half">
+	              <img src="/w3images/forest.jpg" style="width:100%" class="w3-margin-bottom">
+	            </div>
+	            <div class="w3-half">
+	              <img src="/w3images/nature.jpg" style="width:100%" class="w3-margin-bottom">
+	            </div>
+	            <div class="w3-half">
+	              <img src="/w3images/snow.jpg" style="width:100%" class="w3-margin-bottom">
+	            </div>
+	          </div>
+	           </div>
+	         </div>
+	       </div>
+	       <br>
+
+	       <!-- Interests -->
+	       <div class="w3-card w3-round w3-white w3-hide-small">
+	         <div class="w3-container">
+	           <p>Diversity</p>
+	           <p>
+	             <span class="w3-tag w3-small w3-theme-d5">Arthrostylidiinae</span>
+	             <span class="w3-tag w3-small w3-theme-d4">Chusqueinae</span>
+	             <span class="w3-tag w3-small w3-theme-d3">Guaduiinae</span>
+	             <span class="w3-tag w3-small w3-theme-d2">Chusquea</span>
+	             <span class="w3-tag w3-small w3-theme-d1">Merostachys</span>
+	             <span class="w3-tag w3-small w3-theme">Aulonemia</span>
+	             <span class="w3-tag w3-small w3-theme-l1">Arthrostylidium</span>
+	             <span class="w3-tag w3-small w3-theme-l2">Guadua</span>
+	             <span class="w3-tag w3-small w3-theme-l3">Rhipidocladum</span>
+	             <span class="w3-tag w3-small w3-theme-l4">Colanthelia</span>
+	             <span class="w3-tag w3-small w3-theme-l5">Myriocladus</span>
+	           </p>
+	         </div>
+	       </div>
+	       <br>
+
+	       <!-- Alert Box -->
+	       <div class="w3-container w3-display-container w3-round w3-theme-l4 w3-border w3-theme-border w3-margin-bottom w3-hide-small">
+	         <span onclick="this.parentElement.style.display='none'" class="w3-button w3-theme-l3 w3-display-topright">
+	           <i class="fa fa-remove"></i>
+	         </span>
+	         <p><strong>Hey!</strong></p>
+	         <p>This box can be closed.</p>
+	       </div>
+
+	     <!-- End Left Column -->
+	     </div>
+
+	     <!-- Right Column -->
+	     <div class="w3-col m9">
+	       <div class="w3-row-padding">
+	         <div class="w3-col m12">
+	           <div class="w3-card w3-round w3-white">
+	             <div class="w3-container w3-padding">
+
+	               <span class="w3-right w3-opacity">Poaceae: Bambusoideae</span>
+	               <h4>What is a bamboo?</h4><br>
+	               <hr class="w3-clear">
+	               <p>The bamboos (Poaceae: Bambusoideae) are grasses that evolved in and adapted to forest habitats (though some species can now be found in more open environments). There are over 1,500 species of bamboo worldwide, around a third of which are native to the western hemisphere including one genus with three species (<i>Arundinaria</i>; "cane") that is native to what is now the southeastern continental United States.</p>
+	                 <div class="w3-row-padding" style="margin:0 -16px">
+	                   <div class="w3-half">
+	                     <img src="images/uploads/Chusquea_pohlii-cropped.jpg" style="width:100%" alt="Branch of Chusquea species" class="w3-margin-bottom">
+	                   </div>
+	                   <div class="w3-half">
+	                     <img src="images/uploads/Rhipidocladum_clarkiae-BraulioCarrillo.JPG" style="width:100%" alt="Tall bamboo" class="w3-margin-bottom">
+	                 </div>
+	               </div>
+	               <button type="button" class="w3-button w3-theme-d2 w3-margin-bottom">More Info</button>
+
+	             </div>
+	           </div>
+	         </div>
+	       </div>
+
+	       <div class="w3-container w3-card w3-white w3-round w3-margin"><br>
+	 				<?php
+	   				$searchText = 'Search Taxon';
+	   				$buttonText = 'Search';
+	   				include_once($SERVER_ROOT.'/classes/PluginsManager.php');
+	   				$pluginManager = new PluginsManager();
+	           $quicksearch = $pluginManager->quickSearchScript();
+	           $quicksearch .= $pluginManager->quickSearchDiv($searchText,"w3-container w3-padding");
+	       		$quicksearch .= $pluginManager->quickSearchInput("Bambusoideae","w3-padding");
+	           $quicksearch .= $pluginManager->quickSearchButton($buttonText,"w3-button w3-theme-d2 w3-margin-bottom");
+	   				echo $quicksearch;
+	 				?>
+	       </div>
+
+	     <!-- End Right Column -->
+	     </div>
+
+	   <!-- End Grid -->
+	   </div>
+
+	 <!-- End Page Container -->
+	 </div>
+	 <br>
+
 	<?php
 	include($SERVER_ROOT.'/includes/footer.php');
 	?>
 </body>
 </html>
+
+<!-- <div id="quicksearchdiv">
+	<form name="quicksearch" id="quicksearch" action="<?php echo $CLIENT_ROOT; ?>/taxa/index.php" method="get" onsubmit="return verifyQuickSearch(this);">
+		<div id="quicksearchtext" >
+		<?php #echo (isset($LANG['QSEARCH_SEARCH'])?$LANG['QSEARCH_SEARCH']:'Taxon Search'); ?>
+	</div>
+		<input id="taxa" type="text" name="taxon" />
+		<button name="formsubmit"  id="quicksearchbutton" type="submit" value="Search Terms">
+		<?php #echo (isset($LANG['QSEARCH_SEARCH_BUTTON'])?$LANG['QSEARCH_SEARCH_BUTTON']:'Search'); ?>
+	</button>
+	</form>
+</div> -->
