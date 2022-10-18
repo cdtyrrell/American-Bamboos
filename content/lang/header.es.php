7387de615ffff5b1c34121a37deb29beeb511742
Christopher Tyrrell <43977100+cdtyrrell@users.noreply.github.com>
2022-10-18 07:58:08 -0500

    minor changes

---
+++ content/lang/header.es.php
@@ -0,0 +1,53 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+------------------
+*/
+
+$LANG['H_HOME'] = 'Inicio';
+
+$LANG['H_SEARCH'] = 'B&uacute;squeda';
+$LANG['H_BROWSE'] = 'Explorar';
+$LANG['H_COLLECTIONS'] = 'Colecciones';
+$LANG['H_SPECIES'] = 'Especies';
+$LANG['H_SPECIMENS'] = '';
+$LANG['H_MAP'] = 'Mapa';
+$LANG['H_TAXONOMIC_EXPLORER'] = 'Explorador Taxon&oacute;mico';
+$LANG['H_PEOPLE'] = 'Personas';
+$LANG['H_CITE'] = 'Cita Recomendada';
+
+$LANG['H_IMAGES'] = 'Im&aacute;genes';
+$LANG['H_IMAGE_BROWSER'] = 'Navegador de Im&aacute;genes';
+$LANG['H_IMAGE_SEARCH'] = 'B&uacute;squeda de Im&aacute;genes';
+
+$LANG['H_INVENTORIES'] = 'Lista de Especies';
+$LANG['H_PROJECT_1'] = 'Proyecto 1';
+$LANG['H_PROJECT_2'] = 'Proyecto 2';
+
+$LANG['H_DYN_LISTS'] = 'Listados Din&aacute;micos';
+$LANG['H_DYN_KEYS'] = 'Claves de identificación';
+$LANG['H_AMPHIBIA'] = 'Anfibios';
+$LANG['H_AVES'] = 'Aves';
+$LANG['H_FISH'] = 'Peces';
+$LANG['H_INVERTEBRATES'] = 'Invertebrados';
+$LANG['H_MAMMALIA'] = 'Mamíferos';
+$LANG['H_PLANTA'] = 'Plantas';
+$LANG['H_REPTILIA'] = 'Reptiles';
+
+$LANG['H_MORE_INFO'] = 'M&aacute;s Informaci&oacute;n';
+$LANG['H_ABOUT_PROJECT'] = 'Información sobre el Proyecto';
+$LANG['H_ABOUT'] = 'Acerca de';
+$LANG['H_PARTNERS'] = 'Socios';
+$LANG['H_CONTACTS'] = 'Contactos';
+$LANG['H_HELP'] = 'Ayuda de Symbiota';
+$LANG['H_SITEMAP'] = 'Mapa del Sitio';
+
+$LANG['H_WELCOME'] = 'Bienvenido';
+$LANG['H_MY_PROFILE'] = 'Mi Perfil';
+$LANG['H_MY_ACCOUNT'] = 'Mi Cuenta';
+$LANG['H_PROFILE'] = 'Perfil';
+$LANG['H_LOGOUT'] = 'Finalizar Sesi&oacute;n';
+$LANG['H_LOGIN'] = 'Iniciar Sesi&oacute;n';
+$LANG['H_NEW_ACCOUNT'] = 'Nueva Cuenta';
+?>
---
+++ content/lang/index.es.php
@@ -0,0 +1,28 @@
+<?php
+/*
+------------------
+Language: Español (Spanish)
+------------------
+*/
+$LANG['PARDON'] = '¡Por favor, perdone nuestro polen!';
+$LANG['CHECK_BACK'] = 'Este sitio está en desarrollo activo, vuelva a consultar para ver las actualizaciones.';
+$LANG['LAST_UPDATE'] = 'Última Actualización-18 oct 2022';
+
+$LANG['AMERICAN_BAMBOOS'] = 'Bambúes Nativos de <a href=""><span class="display">Abya Yala</span><span class="definition">las Américas</span></a>';
+$LANG['MISSION'] = 'Dedicado a documentar y presentar información sobre la diversidad y distribución de las especies de bambú nativas del hemisferio occidental.';
+$LANG['WHAT_IS_H4'] = '¿Qué es un bambú?';
+$LANG['WHAT_IS'] = 'Los bambúes (Poaceae: Bambusoideae) son gramíneas que evolucionaron y se adaptaron a los hábitats forestales (aunque algunas especies ahora se pueden encontrar en ambientes más abiertos). Hay más de 1.500 especies de bambú en todo el mundo, alrededor de un tercio de las cuales son nativas del hemisferio occidental, incluido un género con tres especies (<i>Arundinaria</i>; "caña") que es nativa de lo que ahora es el sureste de los Estados Unidos continentales.';
+
+
+//Various plugin variables
+$LANG['QSEARCH_SEARCH'] = 'B&uacute;squeda de especies';
+$LANG['QSEARCH_SEARCH_BUTTON'] = 'B&uacute;squeda';
+
+//Slideshow plugin variables
+$LANG['HIDE_CAPTION'] = 'OCULTAR SUBT&Iacute;TULO';
+$LANG['SHOW_CAPTION'] = 'MOSTRAR T&Iacute;TULO';
+$LANG['IMAGE_BY'] = 'Imagen por';
+$LANG['COURTESY_OF'] = 'Cortes&iacute;a de';
+
+$LANG['FOOTER'] = 'El Bambúes Nativos de Abya Yala se basa en la plataforma <a href="https://symbiota.org/" target="_blank">Symbiota</a> personalizada con elementos temáticos de <a href="https://www.w3schools.com/w3css/default.asp" target="_blank">w3.css</a>; modificado y mantenido por <a href="https://github.com/cdtyrrell" target="_blank">Christopher D. Tyrrell</a>';
+?>
