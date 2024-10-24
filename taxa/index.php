231040084f1991c18b5f17c9ccaac004d0252c60
Christopher Tyrrell <43977100+cdtyrrell@users.noreply.github.com>
2024-10-24 14:49:35 -0500

    div fix

--- taxa/index.php
+++ taxa/index.php
@@ -232,13 +232,13 @@ include($SERVER_ROOT.'/includes/header.php');
 						<!-- Dashboard -->
 	        			<div class="dashpanel">
 							<h4>Habitat</h4>
-							<div class="instrument-cluster">
+							<div class="instrument-cluster-quarter">
 								<h5>Elevation Profile</h5>
 								<?php 
 								echo linearGraph(null, array(0,0,0,0,0,0,0,0,0), $taxonManager->getElevations(), array("3500","","2500","","1500","","500"), "elev", FALSE);
 								?>
 							</div>
-							<div class="instrument-cluster">
+							<div class="instrument-cluster-quarter">
 								<h5>Precipitation Profile</h5>
 								<?php
 									$wcdata = $taxonManager->getWC();
@@ -253,7 +253,7 @@ include($SERVER_ROOT.'/includes/header.php');
 									echo linearGraph($wcdata['tavg-avg'], $wcdata['tavg-min'], $wcdata['tavg-max'], $calendarlegend, "temp");
 								?>
 							</div>
-							<div class="instrument-cluster">
+							<div class="instrument-cluster-quarter">
 								<h5>Humidity Profile</h5>
 									<?php
 										echo linearGraph($wcdata['vapr-avg'], $wcdata['vapr-min'], $wcdata['vapr-max'], $calendarlegend, "vapr");
@@ -263,13 +263,13 @@ include($SERVER_ROOT.'/includes/header.php');
 										echo linearGraph($wcdata['srad-avg'], $wcdata['srad-min'], $wcdata['srad-max'], $calendarlegend, "srad");
 									?>
 							</div>
-							<div class="instrument-cluster">
+							<div class="instrument-cluster-quarter">
 								<h5>Wind Speed Profile</h5>
 								<?php
 									echo linearGraph($wcdata['wind-avg'], $wcdata['wind-min'], $wcdata['wind-max'], $calendarlegend, "wind");
 								?>
 							</div>
-							<div class="">
+							<div class="instrument-cluster-full">
 								<p>
 								<?php
 									$habdesc = "<i>".$taxonManager->getTaxonName()."</i> is ";
