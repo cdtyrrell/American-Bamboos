11ddaa22c138c127bd7f01d8b34c200fae776801
Christopher Tyrrell <43977100+cdtyrrell@users.noreply.github.com>
2024-10-29 08:14:34 -0500

    allow git of head + foot

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <p>
+		<?= $LANG['F_POWERED_BY'] ?> <a href="https://symbiota.org/" target="_blank">Symbiota 3.1</a>; modified and maintained by Christopher D. Tyrrell
+	</p>
+</footer>
