528f02467eab4724367d3c5d8fc78ea92d2cceb4
Christopher Tyrrell <43977100+cdtyrrell@users.noreply.github.com>
2024-10-29 09:19:01 -0500

    Update citationcollection.php

--- includes/citationcollection.php
+++ includes/citationcollection.php
@@ -1,4 +1,4 @@
 <?php
 if(!isset($collData) && isset($this->collArr)) $collData = $this->collArr;
 ?>
-<?php echo ($collData['collectionname'])?$collData['collectionname']:"Ada Hayden Herbarium (ISC)"; ?>. Occurrence dataset (ID: <?php echo ($collData['recordid'])?$collData['recordid']:"175"; ?>) <?php echo $collData['dwcaurl']; ?> accessed via the <?php echo ($DEFAULT_TITLE) ? $DEFAULT_TITLE : "American Bamboos"; ?> Portal, <?php echo $SERVER_HOST . $CLIENT_ROOT; ?>, <?php echo date('Y-m-d'); ?>).
+<?php echo ($collData['collectionname'])?$collData['collectionname']:"<<Name of Institution or Collection (ACRONYM)>>"; ?>. Occurrence dataset (ID: <?php echo ($collData['recordid'])?$collData['recordid']:"##"; ?>) <?php echo $collData['dwcaurl']; ?> accessed via the <?php echo ($DEFAULT_TITLE) ? $DEFAULT_TITLE : "American Bamboos"; ?> Portal, <?php echo $SERVER_HOST . $CLIENT_ROOT; ?>, <?php echo date('Y-m-d'); ?>).
