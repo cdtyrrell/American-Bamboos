e9d20381cfe954091903decc474b6fe1b4599a63
Christopher Tyrrell <43977100+cdtyrrell@users.noreply.github.com>
2024-10-29 22:17:43 -0500

    Update autoupdate.php

--- autoupdate.php
+++ autoupdate.php
@@ -1,5 +1,6 @@
 <?php
 // used to automatically pull git updates
+// this file is tethered to a webhook on the github repo
 
 if ( $_POST['payload'] ) {
     shell_exec('cd ~/public_html && git pull');
